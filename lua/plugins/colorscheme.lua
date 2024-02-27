e85013474eac55a563c30d4028f5eb41e3de6376
Kyle Lee <kyle@kylelee.dev>
2024-02-27 15:25:55 -0600

    changed colorscheme to macro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,36 @@
 --my own custom theme forked from lunarvim's darkplus:)
+-- return {
+-- 	dir = "~/dev/darkplus.nvim/",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd("colorscheme darkplus")
+-- 		--set background transparent
+-- 		-- highlight Cursor gui=reverse guifg=NONE guibg=NONE
+-- 		-- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#6e7681", bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#6e7681", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#0f0f0f", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NonText", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
+-- 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
+-- 	end,
+-- }
 return {
-	dir = "~/dev/darkplus.nvim/",
+	dir = "~/dev/colorschemes/",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme darkplus")
-		--set background transparent
-		-- highlight Cursor gui=reverse guifg=NONE guibg=NONE
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+		vim.cmd("colorscheme macro")
+
 		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
@@ -24,14 +47,6 @@ return {
 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
 	end,
 }
--- return {
--- 	dir = "~/dev/colorschemes/",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme macro")
--- 	end,
--- }
 
 -- return {
 -- 	dir = "~/dev/base46-colors/",
