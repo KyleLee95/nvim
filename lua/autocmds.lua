51c34b5f474147c525b82e5712b9bf9df49134f1
Kyle Lee <kyle@kylelee.dev>
2024-03-21 17:29:38 -0500

    added ibl back. added haskell file detection hack to nullls

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -112,3 +112,11 @@ autocmd("LspAttach", {
 		end, opts)
 	end,
 })
+
+-- format on save for haskell
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	-- pattern = "*.hs",
+-- 	callback = function()
+-- 		vim.lsp.buf.format({ async = false })
+-- 	end,
+-- })
--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -3,10 +3,9 @@ return {
 	"nvimtools/none-ls.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "mason.nvim" },
-	opts = function()
+	config = function()
 		local null_ls = require("null-ls")
-		return {
-			-- for list of built-ins: https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+		null_ls.setup({
 			sources = {
 				-- me no know tab or space good or line or newline. let compooper decide for me
 				null_ls.builtins.formatting.stylua,
@@ -16,20 +15,30 @@ return {
 				null_ls.builtins.formatting.shfmt,
 				null_ls.builtins.completion.spell,
 			},
+
 			-- you can reuse a shared lspconfig on_attach callback here
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						group = augroup,
-						buffer = bufnr,
+						pattern = "*",
 						callback = function()
 							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							vim.lsp.buf.format({ bufnr = bufnr })
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							vim.lsp.buf.format({ async = false })
 						end,
 					})
 				end
+
+				-- haskell filetypes are bugged with client.supports_method, so this is a hack
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					pattern = "*.hs",
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
 			end,
-		}
+		})
 	end,
 }
