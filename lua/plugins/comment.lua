36ef83e105be1e7aaf406ad5a9e329d65c55e638
Kyle Lee <kyle@kylelee.dev>
2024-04-13 02:26:54 -0500

    add comment back since bob nightly is broken

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,8 @@
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
 	"numToStr/Comment.nvim",
-	config = function()
-		local comment = require("Comment")
-		comment.setup()
-	end,
+	opts = {
+		-- add any options here
+	},
+	lazy = false,
 }
