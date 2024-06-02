81748b451d6ee6419aecd534a6a3c9bc0ae52281
Kyle Lee <kyle@kylelee.dev>
2024-06-01 20:27:08 -0500

    add supermaven

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,7 +1,7 @@
 return {
 	"Exafunction/codeium.vim",
 	lazy = true,
-	event = 'BufEnter',
+	-- event = 'BufEnter',
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
 		vim.g.codeium_disable_bindings = 1
