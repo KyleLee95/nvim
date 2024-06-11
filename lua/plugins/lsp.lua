364a245d4038ca6cc83d60402f66eb25a6226f84
Kyle Lee <kyle@kylelee.dev>
2024-06-11 18:16:57 -0400

    updated git signs config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,16 +108,18 @@ return {
 		vim.diagnostic.config({
 			virtual_text = false,
 			signs = {
-				[vim.diagnostic.severity.ERROR] = "",
-				[vim.diagnostic.severity.WARN] = "",
-				[vim.diagnostic.severity.INFO] = "",
-				[vim.diagnostic.severity.HINT] = "",
+				text= {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "",
+				}
 			},
 			update_in_insert = false,
 			underline = true,
 			severity_sort = true,
 			float = {
-				focusable = false,
+				focusable = true,
 				style = "minimal",
 				border = "rounded",
 				source = "always",
