6518623f4407937298f0722b8475eb0d7bdda68c
Kyle <kyle@kylelee.dev>
2024-07-09 15:59:49 -0400

    add headlines

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 return {
-	"Mofiqul/vscode.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+	"mofiqul/vscode.nvim",
+	lazy = false,
+	priority = 1000,
 	config = function()
 		require("vscode").setup({
 			transparent = true,
 		})
-
 		vim.cmd("colorscheme vscode")
 		vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#000000", ctermbg = "NONE" })
 	end,
